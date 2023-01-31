48ff82d5b800469b9df27d2691a0faec62b86779
KingBael <www.jayesh567@gmail.com>
2023-01-31 20:49:07 +0530

    added addMenu

--- src/pages/restaurant/manage/menu.tsx
+++ src/pages/restaurant/manage/menu.tsx
@@ -8,6 +8,8 @@ import { useRouter } from "next/router";
 import IconButton from "@mui/material/IconButton";
 import ClearIcon from "@mui/icons-material/Clear";
 import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
 
 const MenuCard = () => {
   return (
@@ -46,8 +48,27 @@ const MenuCard = () => {
   );
 };
 
+const AddCard = () => {
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        p: 2,
+        minWidth: "clamp(15rem,80vw,30rem)",
+        minHeight: "10rem",
+        display: "grid",
+      }}
+    >
+      <Stack direction="row" justifyContent="center" alignItems="center">
+        <AddIcon style={{ fontSize: "2rem" }}></AddIcon>
+      </Stack>
+    </Button>
+  );
+};
+
 export default function managemenu() {
   const router = useRouter();
+  const [someNumber, setSomeNumber] = useState(10);
   return (
     <>
       <Head>
@@ -65,18 +86,20 @@ export default function managemenu() {
         <HomeStructure>
           <Grid
             container
-            p={2}
+            p={"2.5rem 1rem"}
             justifyContent="center"
-            spacing={{ xs: 2, md: 3 }}
+            // spacing={{ xs: 2, md: 3 }}
             gap={2}
             sx={{ overflowY: "scroll" }}
           >
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
+            {/* Assuming Data would be iterated from backend */}
+            {"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem mollitia quia laborum aspernatur non illum. Ex perspiciatis modi ducimus sed quia magnam ab quasi. Laudantium itaque corporis voluptate nihil aspernatur?"
+              .split(" ")
+              .map(() => {
+                return <MenuCard />;
+              })}
+            {/* Add Menu will add to source  */}
+            <AddCard />
           </Grid>
         </HomeStructure>
       </div>
