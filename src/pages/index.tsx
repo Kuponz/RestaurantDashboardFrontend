8cafa34e3b2d6689373c95a6cdaa25aea4e053f5
Om Duragkar <80326954+omduragkar@users.noreply.github.com>
2023-03-22 18:58:52 +0530

    Basic changes in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,7 +96,7 @@ export default function Home() {
         <title>etoPOS</title>
         <meta
           name="description"
-          content="India's first paperless and Innovative POS with minimum investments and Maximum returns"
+          content="etoPOS.. India's first paperless and Innovative POS with minimum investments and Maximum returns"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
