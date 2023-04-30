d82a1ff05d4f23449b1a6c42e21fd91bb4d37c8a
Gayatri Sambare <sambaregayatri2002@gmail.com>
2023-04-30 12:11:49 +0530

    inventory

--- src/pages/restaurant/inventory.tsx
+++ src/pages/restaurant/inventory.tsx
@@ -5,7 +5,8 @@ import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
 import { Button, Stack, Typography } from "@mui/material";
 import { flexBox } from "theme/defaultFunction";
 import { useRouter } from "next/router";
-
+import TopBar from "common/topBar/TopBar";
+import TopBarInventory from "./inventoryy/TopBarInventory";
 export default function inventory() {
   const router = useRouter();
   return (
@@ -23,7 +24,7 @@ export default function inventory() {
         {/* Auth Stuff Here */}
         {/* <Waiter /> */}
         <HomeStructure>
-        <Typography variant="h2">Stay Tuned</Typography>
+          {/* <Typography variant="h2">Stay Tuned</Typography>
           <Typography variant="body2">Construction in Progress!</Typography>
           <Stack direction={"column"} sx={{
               py:5,
@@ -33,10 +34,30 @@ export default function inventory() {
               <Button variant={"outlined"} onClick={()=>{
                   router.push("/restaurant/table")
               }} sx={{...flexBox(), gap:1}}>Book Tables <EastOutlinedIcon/></Button>
-          </Stack>
+          </Stack> */}
+          {/* <Stack direction={"row"}>
+            <TopBar home={true} backUrl={"/"} >
+              <Stack
+                direction={{ xs: "column", md: "row" }}
+                alignItems={"flex-start"}
+                style={{ flex: 1 }}
+                sx={{ paddingInline: "1rem" }}
+                gap={2}
+              >
+                <Stack direction={"row"} gap={2}>
+                  <Button variant='outlined' >Add Expenses</Button>
+                  <Button variant='outlined' >Add Invdfdentory</Button> */}
+          {/* // ! More Intuitive Design Seems to be able to add floor from menu not here */}
+
+          {/* </Stack>
+              </Stack>
+            </TopBar>
+          </Stack> */}
+          {/* <Inventortyy />  */}
+          <TopBarInventory />
 
         </HomeStructure>
-      </div>
+      </div >
     </>
   );
 }
