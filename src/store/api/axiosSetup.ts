81272ce31dc5a24c3bd22283510823a64c4f0aae
Om Duragkar <omaduragkar@gmail.com>
2023-05-23 12:45:36 +0530

    Changing server to new deployment

--- src/store/api/axiosSetup.ts
+++ src/store/api/axiosSetup.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 import { GenericResponse } from "./types";
 
 // const BASE_URL = "http://qaetopos.azurewebsites.net/"; //QA URL
-const BASE_URL = "https://backendetopos.azurewebsites.net/"; //Main URL
+// const BASE_URL = "https://backendetopos.azurewebsites.net/"; //Main URL
+const BASE_URL = "https://etoposbe.azurewebsites.net/"; //Main Current URL
 // const BASE_URL = "http://localhost:5000/"; //LocalHost Url
+
 
 const authApi = axios.create({
   baseURL: BASE_URL,
